1d1d2887f2d407ee52330a71a5d9b90ff195e3aa
tristan <thuckabee.work@gmail.com>
2026-04-16 19:19:42 -0500

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const cookieParser = require('cookie-parser');
 const routes = require('./routes');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
+const { ValidationError } = require('sequelize');
 // ----------------------------------------------------------------------------
 
 const app = express();
@@ -17,30 +18,55 @@ app.use(cookieParser());
 app.use(express.json());
 
 // Security Middleware
-if (!isProduction) {
-  // enable cors only in development
-  app.use(cors());
-}
+if (!isProduction) { app.use(cors()) };
 
 // helmet helps set a variety of headers to better secure your app
-app.use(
-  helmet.crossOriginResourcePolicy({
-    policy: "cross-origin"
-  })
-);
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
 // Set the _csrf token and create req.csrfToken method
-app.use(
-  csurf({
+app.use(csurf(
+  {
     cookie: {
       secure: isProduction,
       sameSite: isProduction && "Lax",
       httpOnly: true
     }
-  })
-);
+  }
+));
 
 app.use(routes); // Connect all the routes
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+
+  next(err);
+})
+app.use((err, _req, _res, next) => {
+  if (err instanceof ValidationError) {
+    let errors = {};
+
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+
+  next(err);
+});
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 // ----------------------------------------------------------------------------
 module.exports = app;
