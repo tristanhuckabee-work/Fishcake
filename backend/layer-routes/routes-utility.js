006f1d6cd0ae8ced710f6c33d8cec25d81688cba
tristan <thuckabee.work@gmail.com>
2026-04-21 20:39:27 -0500

    added user input validation

---
+++ backend/layer-routes/routes-utility.js
@@ -0,0 +1,18 @@
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../util/validation.js");
+// ----------------------------------------------------------------------------
+
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
+
+module.exports = {
+  validateLogin
+}
