ecf5078c50fb5a39b7a87a500ba88ed815ddab15
tristan <thuckabee.work@gmail.com>
2026-04-21 21:30:23 -0500

    Started Frontend

--- backend/layer-routes/index.js
+++ backend/layer-routes/index.js
@@ -14,5 +14,28 @@ router.use("/session", sessionRouter);
 router.use("/tickets", ticketRouter);
 router.use("/users", userRouter);
 
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(path.resolve(__dirname, '../../frontend', 'build', 'index.html'));
+  });
+
+  router.use(express.static(path.resolve("../frontend/build")));
+
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(path.resolve(__dirname, '../../frontend', 'build', 'index.html'));
+  });
+};
+
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+}
+
 // ----------------------------------------------------------------------------
 module.exports = router;
